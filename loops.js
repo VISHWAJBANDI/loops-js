3056412bda555d4b482a07b7c9a317ada565ddad
unknown <vishwajpatel143@gmail.com>
2024-10-25 10:10:41 +0530

    loops statement is created

---
+++ loops.js
@@ -0,0 +1,37 @@
+for(var a=1; a<=10; a++){
+console.log(a)
+}
+// output:1 to 10
+
+for(var a=18; a>=5; a--){
+    console.log(a)
+    }
+    // ot:18 to 5
+     
+var a=2;
+while(a<=12){
+    console.log(a)
+    a++
+}
+// ot:2 to 12
+var a=20;
+while(a>=5){
+    console.log(a)
+    a--
+}
+// ot: 20 to 5
+
+var i=2;
+do{
+    i++
+    console.log(i)
+}while(i<=10)
+    // ot: 3 to 11
+
+    var i=20;
+    do{
+        i--
+        console.log(i)
+    }while(i>=5)
+
+        // ot: 19 to 4
